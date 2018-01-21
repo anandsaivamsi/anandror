8714c2c8e63212b5f5177728f2ebe795980d0c18
anandsaivamsi <anandsaivamsi@gmail.com>
2018-01-21 10:20:34 +0000

    fixedforgotpass

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,9 +2,11 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :port           => '587',
   :address        => 'smtp.sendgrid.net',
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
+
+# heroku config:get SENDGRID_USERNAME
